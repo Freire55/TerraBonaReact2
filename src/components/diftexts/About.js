ca1ef5959ba16d0698ab85a741dd90c335213f29
Tomás <noronha.t@sapo.pt>
2023-02-01 16:38:25 +0000

    About

--- src/components/diftexts/About.js
+++ src/components/diftexts/About.js
@@ -1,11 +1,223 @@
-import React from 'react'
+import React from "react";
+import Footer from "../Footer";
+import NavBar from "../NavBar";
+import Section from "../Section";
+import "./About.css";
+import { Fade } from "react-reveal";
 
 function About() {
   return (
     <>
-    
+      <NavBar />
+      <Section
+        title="Sobre Nós"
+        content="Lorem papspfof asowdpd painoue zixnec eihf  efu fcn"
+        btnTxt="SAIBA MAIS"
+        backgroundImg="/images/TerraBonaBottle.png"
+        path="/about-us"
+      />
+      <div className="sobre">
+        <div className="inicio">
+          <h1>A Nossa História</h1>
+          <h5>Contada Por quem nos leu a alma</h5>
+          <p>***</p>
+        </div>
+        <div className="contentabout container-fluid">
+          <div className="intro">
+            <p>
+              Do vinho da Madeira, o relato mais comum é que regou a
+              independência dos Estados Unidos, o que já não seria coisa pouca.
+              Algumas referências incluem-no também como nota literária em obras
+              como “Henrique IV” de Shakespeare e há quem se lhe refira como o
+              “vinho da eternidade” devido ao seu prazo alargado de consumo.
+            </p>
+            <p>
+              E, em regra, este é “o” vinho da Madeira de que ouvimos falar e
+              que vem nos livros da História.
+            </p>
+            <p>
+              A história que hoje aqui trazemos é outra. Tem pouco mais que um
+              par de anos e, apenas partilha com a ancestral, o facto de ser
+              sobre um vinho que surge por acidente. Falamos do Terra Bona, um
+              vinho branco da Madeira, premiado com medalhas de bronze em dois
+              dos concursos de vinhos mais prestigiados, o IWC e o Decanter, em
+              Londres, e com uma pontuação de 90 pontos pelo conceituado crítico
+              americano Robert Parker.
+            </p>
+          </div>
+          <div className="textimg row">
+            <Fade left>
+            <div className="col-sm-6 p1">
+              <p>
+                Tudo isto num vinho que não estava nos planos de Marco Noronha
+                Jardim e de Maria João Velosa quando compraram a Quinta do
+                Cardo, na freguesia da Boaventura, na Madeira. Aliás, o vinho –
+                ou para sermos mais exatos, a vinha que existia no terreno da
+                quinta, começou por ser um problema.
+              </p>
+              <p>
+                No relato direto: “Este projeto nasce de termos encontrado este
+                terreno e nos apaixonarmos por ele. Ou melhor, eu apaixonei-me e
+                a primeira coisa que fiz foi telefonar à Maria João a dizer ‘vi
+                um terreno fantástico e tens de cá vir. Tem um problema, tem
+                vinha’”, conta Marco Noronha Jardim.
+              </p>
+            </div>
+              </Fade>
+              <Fade right>
+            <div className="col-sm-6">
+              <img
+                className="img-fluid"
+                src="\images\About\16751666127101910404866937953933.jpg"
+                alt=""
+              />
+            </div>
+                </Fade>
+          </div>
+          <Fade bottom>
+          <div className="inicio">
+            <p>
+              Cinco anos depois, o “problema” transformou-se num vinho premiado,
+              elogiado e que mudou o rumo da própria quinta adquirida para
+              propósitos turísticos e agora adaptada a servir também uma vinha e
+              um negócio suportado no vinho. O “projeto de reforma” dos dois
+              bancários transformou-se assim numa aventura pelos vinhos que
+              ainda agora está a começar.
+            </p>
+            <h5>Voltando ao início.</h5>
+            <p>
+              Era uma vez uma quinta, localizada em plena Floresta Laurissilva,
+              património mundial, e que tinha lá plantada uma vinha abandonada
+              com 22 anos em relação à qual os novos proprietários não sabiam
+              bem o que fazer. De conversa em conversa, e tendo presente que
+              queriam fazer do espaço um local privilegiado de agro-turismo,
+              decidiram pedir ajuda e apoio para a recuperar. E em 2016
+              decidiram agarrar a vinha e produzir um vinho: “a nossa ideia era
+              produzir algumas garrafas de vinho para vender no projeto
+              turístico”.
+            </p>
+            <p>
+              Contrataram ajuda profissional, recuperaram a vinha e introduziram
+              um conjunto de atividades “tendencialmemte biológicas” e com
+              respeito pelo meio ambiente. O que é que isso significa?
+              Basicamente “não deitamos remédios nem herbicidas” e “o terreno
+              está ¾ do ano em biológico e entre abril e agosto é que há um
+              conjunto de tratamentos com produtos fitossanitários”. Daqui
+              resulta que sejam “o primeiro produtor com as vinhas licenciadas
+              em produção integrada”.
+            </p>
+          </div>
+          </Fade>
+          <div className="textimg row">
+          <Fade left>
+            <div className="col-sm-6">
+              <img
+                className="img-fluid"
+                src="\images\About\16751666227852458220409249260417.jpg"
+                alt=""
+                />
+            </div>
+                </Fade>
+                <Fade right>
+            <div className="col-sm-6">
+              <p>
+                Mas não percamos de vista o vinho. Em 2017, Marco e Maria João
+                têm então o seu primeiro vinho. Tecnicamente um produto da casta
+                Arnsburger através de um cruzamento com origem na casta
+                Riesling. Para leigos, a casta alemã Arnsburger é muito
+                semelhante aos ditos Rieslings, castas da Alsácia francesa de
+                onde resultam vinhos muito aromáticos – e já bem conhecidos
+                mundo fora.
+              </p>
+              <p>
+                “Nós fizemos um vinho com o que tínhamos, introduzimos 10% de
+                verdelho de umas vinhas de uma tia da Maria João”, conta Marco,
+                “a nossa ideia era que o vinho fosse bebível, com toda a
+                franqueza”.
+              </p>
+            </div>
+                </Fade>
+          </div>
+          <Fade bottom>
+          <div className="inicio">
+            <p>
+              De bebível, o vinho – branco - da vinha abandonada passou a
+              coqueluche junto a amigos e pessoas a quem iam dando a provar.
+              Mas, na dúvida – seria gentileza? – Marco e Maria João decidiram
+              “avaliar ao mais alto nível” aquilo que “toda a gente dizia que
+              era maravilhoso”. E é aí que surgem os concursos de vinho em
+              Londres e a avaliação de Robert Parker. Foi a prova que o vinho
+              era, de facto, bom, e também que “na Madeira podem fazer-se bons
+              vinhos brancos”.
+            </p>
+            <p>
+              A partir daí o projeto transformou-se. As unidades turísticas,
+              cuja construção se deverá iniciar entre novembro deste ano e
+              Janeiro de 2020, reduziram-se de seis para cinco para preservar ao
+              máximo as vinhas existentes e os proprietários da Quinta do Cardo
+              decidiram criar uma micro-adega para produção de três vinhos
+              distintos, 1500 garrafas não mais. “Pequenas produções, pequenos
+              lotes de outros vinhos com parcerias de outros produtores da
+              região”, dizem.
+            </p>
+          </div>
+          </Fade>
+          <div className="textimg row">
+            <Fade left>
+            <div className="col-sm-6">
+              <p>
+                Nestes três anos, os dois bancários que procuravam um projeto de
+                vida alternativo, aprenderam também muito sobre a vinha e o
+                vinho. “Nós acreditamos é que é possível fazer excelentes vinhos
+                na Madeira e que é preciso ter o controlo da produção da uva”,
+                afirmam. Este ano, os dois selecionaram uvas bago a bago,
+                tiraram todos os bagos estragados de todos os cachos. No dia
+                seguinte começaram a vindima às 5 da manhã para evitar maturação
+                da uva depois de apanhada e antes de entrar na adega. Assim,
+                dizem, “toda a uva que entra na adega é uva boa e isso tem um
+                impacto brutal no produto final”.
+              </p>
+            </div>
+              </Fade>
+              <Fade right>
+            <div className="col-sm-6">
+              <img
+                className="img-fluid"
+                src="\images\About\16751666329398562617980004722017.jpg"
+                alt=""
+                />
+            </div>
+                </Fade>
+          </div>
+          <div className="inicio">
+            <p>
+              O projeto turístico também mudou – será turismo mas ligado à
+              vinha, batizado em conformidade como Terra Bona Nature &
+              Vineyards. Mas tudo se manterá pequeno, quase artesanal. “Temos
+              como compromisso ter um terço da produção para o mercado de Lisboa
+              e Porto, um terço para mercado regional e um terço para nós”.
+            </p>
+            <p>
+              Nasceu assim o ‘Terra Bona’ Family Harvest, uma história
+              improvável de um vinho branco da Madeira.
+            </p>
+            <p>
+              Uma vinha pequena, uma uva escolhida à mão. “É assim que é e não
+              vai mudar”, dizem os donos.
+            </p>
+            <h5>Rute Sousa Vasco e Rodrigo Moreira Rato</h5>
+            <h5>in "Sapo24" 10.09.2019</h5>
+            <div className="thanks">
+              <h4>"O nosso muito obrigado a ambos.</h4>
+              <h4>Assim começou a nossa história..."</h4>
+              <a href="https://24.sapo.pt/economia/artigos/a-ideia-era-um-projeto-de-vida-para-a-reforma-o-resultado-foi-um-vinho-premiado-nos-melhores-concursos-internacionais" target="_blank">Ler o artigo</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default About
+export default About;
